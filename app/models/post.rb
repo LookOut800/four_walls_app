a3f0daec0d915eb5efafdff489802f9398a34caa
Charles Talbot <charlietalbot@gmail.com>
2015-02-07 13:50:15 -0500

    added image attachment to post model, and migrate

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
-  has_many :pictures
+  has_attached_file :image, styles: { large: "600x600>", medium: "400x400>", thumbnail: "150x150#" }
 
   validates :title, :body, presence: true
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
