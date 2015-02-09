bb9d16f40b73f453cddcdb2489a8c5449e49b160
Charles Talbot <charlietalbot@gmail.com>
2015-02-09 13:06:53 -0500

    edit model to include paperclip paths

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,11 @@
 class Post < ActiveRecord::Base
   has_many :comments
-  has_attached_file :image, styles: { large: "800x800>", medium: "400x400>", thumbnail: "150x150#" }
-
+  has_attached_file :image, styles: { large: "800x800>", medium: "400x400>", thumbnail: "150x150#" },
+      :bucket => 'fourthwallappbucket',
+                    :s3_credentials => {
+                      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+                      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+                    }
 
 
   validates :title, :body, presence: true
